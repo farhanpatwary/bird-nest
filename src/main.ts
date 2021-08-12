6a6a350dae8101fad48004ff6920a36a53e54117
Farhan Patwary <patwary.farhan87@gmail.com>
2021-08-12 23:35:28 +0100

    Fixed env imports

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
+// dotenv needs to be imported before anything, db connection will fail otherwise
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+// Other imports
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
 import { createConnection } from 'typeorm';
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 import { User } from './entities/user/user.entity';
 
 async function bootstrap() {
-  dotenv.config();
-
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
