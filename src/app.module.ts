edc00f6df0bb148df8360255a0ba4df54c327de2
Farhan Patwary <patwary.farhan87@gmail.com>
2021-08-12 21:23:14 +0100

    Removed db service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from './controllers/user/user.controller';
-import { DatabaseService } from './providers/database/database.service';
 
 @Module({
   imports: [],
   controllers: [AppController, UserController],
-  providers: [AppService, DatabaseService],
+  providers: [AppService],
 })
 export class AppModule {}
