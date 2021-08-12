6f110424d6087bf29b88dcd044271bbac8c5b85a
Farhan Patwary <patwary.farhan87@gmail.com>
2021-08-12 23:06:29 +0100

    Created getAll method for user service

--- src/entities/user/user.service.ts
+++ src/entities/user/user.service.ts
@@ -1,6 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
 export class UserService {
-  constructor() {}
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  getAll(): Promise<User[]> {
+    return this.userRepository.find();
+  }
 }
