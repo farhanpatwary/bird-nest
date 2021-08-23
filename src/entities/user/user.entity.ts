993b769e5f44a5fbd4f73fad4f3bde6dc8832705
Farhan Patwary <patwary.farhan87@gmail.com>
2021-08-23 10:25:48 +0100

    Added User role and crud dates

--- src/entities/user/user.entity.ts
+++ src/entities/user/user.entity.ts
@@ -1,4 +1,12 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { type } from 'os';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity()
 export class User {
@@ -11,8 +19,22 @@ export class User {
   @Column()
   lastName: string;
 
+  @Column({ type: 'enum', enum: ['customer', 'admin'], default: 'customer' })
+  role: UserRoleType;
+
   @Column({
     default: true,
   })
   isActive: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
 }
+
+export type UserRoleType = 'customer' | 'admin';
