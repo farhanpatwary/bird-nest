2c187e078d298d08d39c71b3b7b8295b906890a0
Farhan Patwary <patwary.farhan87@gmail.com>
2021-08-26 16:19:35 +0100

    Moved User entity to user folder

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,5 +1,5 @@
 import { Item } from 'src/entities/item/item.entity';
-import { User } from 'src/entities/user/user.entity';
+import { User } from 'src/controllers/user/entity/user.entity';
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 
 // ORM DB connection
--- src/controllers/user/user.controller.ts
+++ src/controllers/user/user.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, Param, Post, Put, Req, Res } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-import { UserService } from 'src/entities/user/user.service';
+import { UserService } from 'src/controllers/user/entity/user.service';
 
 @ApiTags('Users')
 @Controller('users')
